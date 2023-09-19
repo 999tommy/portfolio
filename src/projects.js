5a73a40dfbb5834afae59305078c04699586f71c
999tommy <adegboyetommy@gmail.com>
2023-09-19 12:16:11 +0100

    Update projects.js

--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,5 @@
 const projects = [
-    {id: 1, name: 'BookHub', description: 'BookHub is a community created to link book lovers with their favorite books as well as help them get in contact with their favorite writers, Its a wonderful place to updated on the latest books', image: '/src/assets/bookhub.png', linkTo: './Interior'},
+    {id: 1, name: 'BlogApp', description: 'BLogApp is a community created to link bloggers anad readers them get in contact with their favorite writers, Its a wonderful place to updated on the latest news', image: '/src/assets/bookhub.png', linkTo: 'https://blopapp.onrender.com'},
     {id: 2, name: 'FashionHut', description: 'FashionHut is a clothing an accessories e-commerce store connecting buyers and sellers together. Its main purpose is to market products and make income ', image: '/src/assets/fashionhut1.png', linkTo: './Realestate'},
     {id: 3, name: 'FastEat', description: "FastEat is a meal recipe Generating App with the use of API. It was a great experience for me to build the entire frontend and work with API's.", image: '/src/assets/fasteat.png', linkTo: 'https://fasteat-recipe.onrender.com'},
     {id: 4, name: 'RentUp', description:'This is a real-estate web app for marketing houses which anyone can use to present their business online.', image:'/src/assets/real-estate.png', linkTo: 'https://rentup-u8xl.onrender.com'},
