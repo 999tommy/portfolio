0d7a18f9ca6c4b4475da5c666b291b215ed9a620
999tommy <adegboyetommy@gmail.com>
2023-09-19 09:36:24 +0100

    update projectjs

--- src/projects.js
+++ src/projects.js
@@ -3,6 +3,7 @@ const projects = [
     {id: 2, name: 'FashionHut', description: 'FashionHut is a clothing an accessories e-commerce store connecting buyers and sellers together. Its main purpose is to market products and make income ', image: '/src/assets/fashionhut1.png', linkTo: './Realestate'},
     {id: 3, name: 'FastEat', description: "FastEat is a meal recipe Generating App with the use of API. It was a great experience for me to build the entire frontend and work with API's.", image: '/src/assets/fasteat.png', linkTo: 'https://fasteat-recipe.onrender.com'},
     {id: 4, name: 'RentUp', description:'This is a real-estate web app for marketing houses which anyone can use to present their business online.', image:'/src/assets/real-estate.png', linkTo: './Interior'}
+    {id: 5, name: 'DefiApp', description:'This app is used to monitor th crpto market and teach us on decentralized finance.', image:'/src/assets/real-estate.png', linkTo: 'https://app-defi.onrender.com'}
 ]
 
 export default projects;
