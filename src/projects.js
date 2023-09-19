53c37733ca56bb1d88e74d299ae323e75e49d52b
999tommy <adegboyetommy@gmail.com>
2023-09-19 11:00:34 +0100

    Update

--- src/projects.js
+++ src/projects.js
@@ -2,7 +2,7 @@ const projects = [
     {id: 1, name: 'BookHub', description: 'BookHub is a community created to link book lovers with their favorite books as well as help them get in contact with their favorite writers, Its a wonderful place to updated on the latest books', image: '/src/assets/bookhub.png', linkTo: './Interior'},
     {id: 2, name: 'FashionHut', description: 'FashionHut is a clothing an accessories e-commerce store connecting buyers and sellers together. Its main purpose is to market products and make income ', image: '/src/assets/fashionhut1.png', linkTo: './Realestate'},
     {id: 3, name: 'FastEat', description: "FastEat is a meal recipe Generating App with the use of API. It was a great experience for me to build the entire frontend and work with API's.", image: '/src/assets/fasteat.png', linkTo: 'https://fasteat-recipe.onrender.com'},
-    {id: 4, name: 'RentUp', description:'This is a real-estate web app for marketing houses which anyone can use to present their business online.', image:'/src/assets/real-estate.png', linkTo: './Interior'},
+    {id: 4, name: 'RentUp', description:'This is a real-estate web app for marketing houses which anyone can use to present their business online.', image:'/src/assets/real-estate.png', linkTo: 'https://rentup-u8xl.onrender.com'},
     {id: 5, name: 'DefiApp', description:'This app is used to monitor the crypto market and teach us on decentralized finance.', image:'/src/assets//defiapp.png', linkTo: 'https://app-defi.onrender.com'},
     {id: 6, name: 'InteriorDesign', description:'A beautiful animated Website made with React and Tailwindcss with the purpose of shopping for interior designs for household', image:'/src/assets/interior.png', linkTo: 'https://interiordesign-store.onrender.com'}
 ]
